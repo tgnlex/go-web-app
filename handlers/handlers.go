06735646f81d4a662870aa235b0a21a83b26a9ed
tgnlex <alexanderwebber76@gmail.com>
2024-05-03 17:14:30 -0700

    created basic form handler

--- handlers/handlers.go
+++ handlers/handlers.go
@@ -1,12 +1,26 @@
 package handlers
 
 import (
+	"fmt"
 	"html/template"
 	"net/http"
 )
 
 var tpl = template.Must(template.ParseFiles("static/index.html"))
 
-func IndexHandler(w http.ResponseWriter, r *http.Request) {
+func IndexHandle(w http.ResponseWriter, r *http.Request) {
 	tpl.Execute(w, nil)
+}
+
+func FormHandle(w http.ResponseWriter, r *http.Request) {
+	ctx := r.Context()
+
+	fmt.Printf("%s: got /hello request \n"ctx.Value())
+
+	val := r.PostFormValue("val")
+	if val == "" {
+		w.Header().Set("x-missing-field", "val")
+		w.WriteHeader(http.StatusBadRequest)
+	}
+	io.WriteString(w, fmt.Sprintf("Hello, %s!\n", val))
 }
