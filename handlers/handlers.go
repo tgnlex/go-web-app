0eef851980c133c74fbb31d2e96c479eb89d0635
tgnlex <alexanderwebber76@gmail.com>
2024-05-07 23:55:14 -0700

    refactoring

--- handlers/handlers.go
+++ handlers/handlers.go
@@ -1,6 +1,7 @@
 package handlers
 
 import (
+	"bytes"
 	"fmt"
 	"html/template"
 	"io"
@@ -8,17 +9,28 @@ import (
 )
 
 var tpl = template.Must(template.ParseFiles("static/index.html"))
+var buf = &bytes.Buffer{}
+var BufOut = buf.WriteTo
+var ServerError = http.StatusInternalServerError
 
-func IndexHandle(w http.ResponseWriter, r *http.Request) {
-	tpl.Execute(w, nil)
+type Res = http.ResponseWriter
+type Req = *http.Request
+
+func IndexHandle(w Res, r Req) {
+	err := tpl.Execute(w, nil)
+	if err != nil {
+		http.Error(w, err.Error(), ServerError)
+		return
+	}
+	BufOut(w)
 }
 
-func FormHandle(w http.ResponseWriter, r *http.Request) {
+func FormHandle(w Res, r Req) {
 	ctx := r.Context()
-
-	fmt.Printf("%s: got /hello request \n", ctx.Value)
-
 	val := r.PostFormValue("val")
+
+	fmt.Printf("%s: got /hello request \n", ctx.Value(val))
+
 	if val == "" {
 		w.Header().Set("x-missing-field", "val")
 		w.WriteHeader(http.StatusBadRequest)
--- main.go
+++ main.go
@@ -2,39 +2,33 @@ package main
 
 import (
 	"fmt"
-	"log"
 	"net/http"
 	"os"
-	"time"
 
 	"example.com/project/handlers"
-	"github.com/joho/godotenv"
+	"example.com/project/lib"
 )
 
-func main() {
-	// Aliases
-	print := fmt.Printf
-	handle := http.HandleFunc
-	timestamp := time.Now()
-	listen := http.ListenAndServe
-	warn := log.Fatal
-	env := os.Getenv
-	logline := log.Println
-	// Load godotenv module
-	err := godotenv.Load()
-	if err != nil {
-		logline("Error loading .env file")
-	}
-	// Get Enviornment Variables
-	port := env("PORT")
-	// Register Route Handlers
-	handle("/", handlers.IndexHandle)
-	handle("/submit", handlers.FormHandle)
-	// Start Server
-	print("[GO]: Starting http server at port %s \n", port)
-	print("[GO]: Time {%s}", timestamp)
-	if err := listen(port, nil); err != nil {
-		warn(err)
-	}
+// Aliases
+var Handle = http.HandleFunc
+var Env = os.Getenv
+var Print = fmt.Printf
 
+// Lib
+var LoadEnv = lib.LoadEnv
+var Server = lib.Server
+
+// Handles
+var Index = handlers.IndexHandle
+var Form = handlers.FormHandle
+
+func Main() {
+	LoadEnv()
+	// Get Enviornment Variables
+	PORT := Env("PORT")
+	// Register Route Handlers
+	Handle("/", Index)
+	Handle("/submit", Form)
+	// Start Server
+	Server(PORT)
 }
